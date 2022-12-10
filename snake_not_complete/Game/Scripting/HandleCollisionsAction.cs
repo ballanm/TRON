46b0d62a5d337bd5dd5ea9b029ea1eb42f79e3b4
ballanm <113638662+ballanm@users.noreply.github.com>
2022-12-10 11:44:49 -0700

    Add files via upload

---
+++ snake_not_complete/Game/Scripting/HandleCollisionsAction.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Unit05.Game.Casting;
+using Unit05.Game.Services;
+
+
+namespace Unit05.Game.Scripting
+{
+    /// <summary>
+    /// <para>An update action that handles interactions between the actors.</para>
+    /// <para>
+    /// The responsibility of HandleCollisionsAction is to handle the situation when the snake 
+    /// collides with the food, or the snake collides with its segments, or the game is over.
+    /// </para>
+    /// </summary>
+    public class HandleCollisionsAction : Action
+    {
+        private bool isGameOver = false;
+
+        /// <summary>
+        /// Constructs a new instance of HandleCollisionsAction.
+        /// </summary>
+        public HandleCollisionsAction()
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Execute(Cast cast, Script script)
+        {
+            if (isGameOver == false)
+            {
+                HandleSegmentCollisions(cast);
+                HandleGameOver(cast);
+            }
+        }
+        /// use Raylib gettime for tail length call it growtail
+
+        /// <summary>
+        /// Updates the score nd moves the food if the snake collides with it.
+        /// </summary>
+        /// <param name="cast">The cast of actors.</param>
+
+        /// <summary>
+        /// Sets the game over flag if the snake collides with one of its segments.
+        /// </summary>
+        /// <param name="cast">The cast of actors.</param>
+        private void HandleSegmentCollisions(Cast cast)
+        {
+            Snake snake = (Snake)cast.GetFirstActor("snake");
+            Actor head = snake.GetHead();
+            List<Actor> body = snake.GetBody();
+
+            foreach (Actor segment in body)
+            {
+                if (segment.GetPosition().Equals(head.GetPosition()))
+                {
+                    isGameOver = true;
+                }
+            }
+        }
+
+        private void HandleGameOver(Cast cast)
+        {
+            if (isGameOver == true)
+            {
+                Snake snake = (Snake)cast.GetFirstActor("snake");
+                List<Actor> segments = snake.GetSegments();
+
+                // create a "game over" message
+                int x = Constants.MAX_X / 2;
+                int y = Constants.MAX_Y / 2;
+                Point position = new Point(x, y);
+
+                Actor message = new Actor();
+                message.SetText("Game Over!");
+                message.SetPosition(position);
+                cast.AddActor("messages", message);
+
+                // make everything white
+                foreach (Actor segment in segments)
+                {
+                    segment.SetColor(Constants.WHITE);
+                }
+                food.SetColor(Constants.WHITE);
+            }
+        }
+
+    }
+}
